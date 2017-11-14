83efae6fef8c5c1a797cbfaaebb3d69a1108436b
berkbid <berkbid@uw.edu>
2017-11-13 20:59:57 -0700

    refactored some code

--- drawingwidget.cpp
+++ drawingwidget.cpp
@@ -11,16 +11,10 @@ DrawingWidget::DrawingWidget(QWidget *parent) : QWidget(parent),
 
     horizontalScroll = 0;
     verticalScroll = 0;
-
-
     scaleFactor = 1;
-    //Default Gray drawing board, Can set variable size here.
-    //currImage = QPixmap(500, 500).toImage();
 
     currImage.fill(Qt::transparent);
     tempImage = currImage;
-
-
 }
 
 void DrawingWidget::scrollHor(int change) {
@@ -36,12 +30,8 @@ void DrawingWidget::scrollVer(int change) {
 void DrawingWidget::createNewBoard(int w, int h) {
     scaleFactor = 1;
     currImage.fill(Qt::transparent);
-
-    //QImage temp(w, h, QImage::Format_ARGB32);
-
     currImage= QImage(w, h,  QImage::Format_ARGB32 );
     tempImage = currImage;
-
 }
 
 
@@ -57,18 +47,9 @@ void DrawingWidget::paintEvent(QPaintEvent *e) {
     //By that i mean change tempimage.width for sw, and height for sh, which should change
     //what portion of draw we are at.
     //if defscale and draw scaled image..
-
-
-
     QPainter painter(this);
 
-    //if(defScale)
-
-        //qDebug() << tempImage.size().width() << "-" << tempImage.size().height();
     painter.drawImage(0, 0, tempImage, horizontalScroll, verticalScroll, 0, 0);
-    //else
-       // painter.drawImage(0, 0, tempImage.scaled(200, 200, Qt::IgnoreAspectRatio), 0, 0);
-
 }
 
 void DrawingWidget::mousePressEvent(QMouseEvent *e) {
@@ -80,29 +61,15 @@ void DrawingWidget::mouseReleaseEvent(QMouseEvent *e) {
 }
 
 void DrawingWidget::resizeEvent(QResizeEvent *e) {
-
-
-
-    //scale.setWidth(w);
-    //scale.setHeight(h);
-    //std::cout << w << "-wid, height - " << h << " ";
-
-    //currImage = currImage.scaled(scale, Qt::KeepAspectRatio);
-
-    //emit rescale(scale);
-
 }
 
 
 
 void DrawingWidget::setDefinitiveScale(int w, int h) {
-
 }
 
 
 void DrawingWidget::scaleIn(int passScaleFactor) {
-
-
     scaleFactor /= passScaleFactor;
 
     currImage.setDevicePixelRatio(scaleFactor);
@@ -129,5 +96,3 @@ void DrawingWidget::highlightPixel(QPoint point) {
     tempImage.setPixelColor(point, currColor);
     update();
 }
-
-
--- main.cpp
+++ main.cpp
@@ -2,16 +2,10 @@
 #include <QApplication>
 #include <QDebug>
 
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-
-    qDebug() << argv[0];
-
     Model m;
     View v(&m);
     v.show();
-
     return a.exec();
 }
